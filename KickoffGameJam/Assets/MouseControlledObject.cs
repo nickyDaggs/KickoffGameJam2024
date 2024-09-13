ccb3e38d287a6de9c2b0ba93a2a3415d2464e91f
nickyDaggs <49346031+nickyDaggs@users.noreply.github.com>
2024-09-13 16:51:52 -0400

    MiniGames Basis

---
+++ KickoffGameJam/Assets/MouseControlledObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControlledObject : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    public bool moving;
+
+    public float xOffset;
+    public float yOffset;
+    // Start is called before the first frame update
+    public void StartMove()
+    {
+        moving = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moving)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
+            transform.position = new Vector2(mousePosition.x + xOffset, mousePosition.y + yOffset);
+        }
+    }
+}
--- KickoffGameJam/Assets/PipePuzzleManager.cs
+++ KickoffGameJam/Assets/PipePuzzleManager.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PipePuzzleManager : MonoBehaviour
 {
-    public List<Animator> correctPipes;
+    [System.Serializable]
+    public class Pipe
+    {
+        public Transform pipeObject;
+        public int turns = 0;
+        public int turnLimit = 0;
+    }
+
+    public List<Pipe> pipesStored;
+    public List<int> correctPipesValues;
+
+    Sprite pipeFlat;
+
+    public float RotationSpeed = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +28,72 @@ public class PipePuzzleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i = 0; i < pipesStored.Count; i++)
+        {
+            int number = i;
+
+            pipeRandPress(number);
+        }
     }
 
-    public void pipePress(Animator anim)
+    public void pipePress(int pipeNum)
     {
-        anim.SetTrigger("Turn");
+        //anim.SetTrigger("Turn");
+        //Quaternion.Lerp(transform.rotation, transform.rotation + 90, Time.time * RotationSpeed);
+        Pipe change = pipesStored[pipeNum];
+
+        change.turns++;
+        
+        if (change.turns <= change.turnLimit)
+        {
+            change.pipeObject.Rotate(0, 0, 90);
+        } else
+        {
+            change.turns = 0;
+            change.pipeObject.rotation = Quaternion.identity;
+        }
+
+
+        CheckPipes();
+    }
+
+    void pipeRandPress(int pipeNum)
+    {
+        //anim.SetTrigger("Turn");
+        //Quaternion.Lerp(transform.rotation, transform.rotation + 90, Time.time * RotationSpeed);
+        Pipe change = pipesStored[pipeNum];
+
+        change.turns = Random.Range(0, pipesStored[pipeNum].turnLimit + 1);
+
+        for(int i = 0; i < change.turns; i++)
+        {
+            change.pipeObject.Rotate(0, 0, 90);
+        }
+
+        CheckPipes();
+    }
+
+    void CheckPipes()
+    {
+        int sum = 0;
+        for(int i = 0; i < correctPipesValues.Count; i++)
+        {
+            if (pipesStored[i].turns >= correctPipesValues[i])
+            {
+                sum++;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if(sum == correctPipesValues.Count)
+        {
+            foreach(Pipe pipeObj in pipesStored)
+            {
+                pipeObj.pipeObject.GetComponent<Button>().interactable = false;
+            }
+        }
     }
 }
